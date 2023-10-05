ccd066290a8ef36c859899556a237d4987c0cd72
BB-LuanTran <luan.tran@blackbook.ai>
2023-10-05 23:22:19 +0700

    update

--- src/socket/socket.service.ts
+++ src/socket/socket.service.ts
@@ -16,6 +16,9 @@ export class SocketGateway
   @WebSocketServer()
   server: Server;
 
+  driverSocket: Socket = null;
+  passengerSocket: Socket = null;
+
   afterInit(server: any) {
     console.log('Init gateway');
     this.server.on('connection', (socket) => {
@@ -31,7 +34,38 @@ export class SocketGateway
       message: 'Welcome to the socket.io server!',
       userCount: this.server.engine.clientsCount, // Number of connected clients
     };
-    socket.emit('welcome', connectData);
+
+    socket.on('passengerRequest', (data) => {
+      this.driverSocket = socket;
+      console.log('passengerRequest', data);
+    });
+
+    socket.on('driverRequest', (data) => {
+      this.passengerSocket = socket;
+      if (!this.driverSocket) return;
+      console.log('driverRequest', data);
+      this.driverSocket.emit('driverRequest', data);
+
+      this.passengerSocket.on('passengerDisconnect', () => {
+        console.log('passengerDisconnect');
+        this.driverSocket.emit('passengerDisconnect');
+        this.driverSocket.disconnect();
+        this.passengerSocket.disconnect();
+      });
+    });
+
+    socket.on('acceptPassenger', (data) => {
+      if (!this.passengerSocket) return;
+      console.log('driverLocation', data);
+      this.passengerSocket.emit('acceptPassenger', data);
+
+      this.driverSocket.on('driverDisconnect', () => {
+        console.log('driverDisconnect');
+        this.passengerSocket.emit('driverDisconnect');
+        this.passengerSocket.disconnect();
+        this.driverSocket.disconnect();
+      });
+    });
   }
 
   handleDisconnect(socket: Socket) {
